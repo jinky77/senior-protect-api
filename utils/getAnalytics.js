ea6459cb64e71ed271b52b54d2ed70f7fc821a92
Paul Levent <paullevent2014@u.northwestern.edu>
2024-10-17 15:01:10 +0200

    renamed properties to match gifted-charts expectations

--- utils/getAnalytics.js
+++ utils/getAnalytics.js
@@ -6,48 +6,39 @@ const capitalizeFirstLetter = (string) => {
 };
 
 const getAnalytics = (data, config, timeframe) => {
-  const { startOf, increment, isSame, format: formatStr, count } = config;
+  const { startOf, increment, format: formatStr, count } = config;
   const start = startOf(new Date());
 
   return Array.from({ length: count }, (_, i) => {
     const periodStart = increment(start, i);
     const periodEnd = increment(periodStart, 1);
 
-    let formattedPeriod;
+    let formattedLabel;
     switch (timeframe) {
       case "day":
-        formattedPeriod = capitalizeFirstLetter(format(periodStart, "EEEE d MMMM", { locale: fr }));
+        formattedLabel = capitalizeFirstLetter(format(periodStart, "EEEE d MMMM", { locale: fr }));
         break;
       case "week":
-        formattedPeriod = capitalizeFirstLetter(format(periodStart, "EEEE d MMMM", { locale: fr }));
+        formattedLabel = capitalizeFirstLetter(format(periodStart, "EEEE d MMMM", { locale: fr }));
         break;
       case "month":
-        const weekEnd = endOfWeek(periodStart, { weekStartsOn: 1 });
-        formattedPeriod = `Du ${format(periodStart, "dd", { locale: fr })} au ${format(weekEnd, "dd MMMM", { locale: fr })}`;
+        formattedLabel = `Du ${format(periodStart, formatStr, { locale: fr }).toLowerCase()} au ${format(addDays(periodEnd, -1), formatStr, {
+          locale: fr,
+        }).toLowerCase()}`;
         break;
       case "year":
-        formattedPeriod = capitalizeFirstLetter(format(periodStart, "MMMM yyyy", { locale: fr }));
+        formattedLabel = capitalizeFirstLetter(format(periodStart, "MMMM yyyy", { locale: fr }));
         break;
       default:
-        formattedPeriod = format(periodStart, formatStr, { locale: fr });
+        formattedLabel = format(periodStart, formatStr, { locale: fr });
     }
 
-    const events = Object.values(data).filter((event) => isWithinInterval(new Date(event.startTime), { start: periodStart, end: periodEnd })).length;
-
-    // return {
-    //   period:
-    //     timeframe === "month"
-    //       ? `${format(periodStart, formatStr, { locale: fr }).toLowerCase()} - ${format(addDays(periodEnd, -1), formatStr, {
-    //           locale: fr,
-    //         }).toLowerCase()}`
-    //       : format(periodStart, formatStr, { locale: fr }).toLowerCase(),
-    //   events,
-    // };
+    const value = Object.values(data).filter((event) => isWithinInterval(new Date(event.startTime), { start: periodStart, end: periodEnd })).length;
 
     return {
-      period: format(periodStart, formatStr, { locale: fr }),
-      formattedPeriod,
-      events,
+      label: format(periodStart, formatStr, { locale: fr }),
+      formattedLabel,
+      value,
     };
   });
 };
